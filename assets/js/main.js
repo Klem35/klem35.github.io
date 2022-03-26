78bcdde986941dd78cf12c6b4b8efb5e3489833e
Clément Huchet <cl.huchet35@gmail.com>
2022-03-26 18:12:39 +0100

    Updating portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,39 @@ window.onscroll = function() {
   }
 };
 
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target)
+
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('qualification_active')
+    })
+    target.classList.add('qualification_active')
+    tabs.forEach(tab => {
+      tab.classList.remove('qualification_active')
+    })
+    tab.classList.add('qualification_active')
+  })
+})
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+let swiperPortfolio = new Swiper('.portfolio_container', {
+  cssMode: true,
+  loop: false,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+})
+
 
 
 
